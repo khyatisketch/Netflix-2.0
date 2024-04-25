5f3fef811c647002d442a92db94c0aaedb74f090
Khyati-Agarwal <84928524+khyatisketch@users.noreply.github.com>
2024-04-25 09:51:38 +0530

    Add files via upload

---
+++ netflix/src/components/VideoBackground.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import useMovieById from '../hooks/useMovieById'
+import { useSelector } from 'react-redux';
+
+const VideoBackground = ({ movieId }) => {
+    const trailerMovie = useSelector(store => store.movie.trailerMovie);
+    useMovieById(movieId);
+
+    return (
+        <div className=' w-screen'>
+            <iframe
+                className={`w-screen aspect-video`}
+                src={`https://www.youtube.com/embed/${trailerMovie?.key}?si=gy_MRjR_i_KjNERa&autoplay=1&mute=1`}
+                title="YouTube video player"
+                frameBorder="0"
+                allowFullScreen>
+            </iframe>
+        </div>
+    )
+}
+
+export default VideoBackground
