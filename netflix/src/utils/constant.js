6113dc957f498005920f71fc6ef2873b35a81ac6
Khyati-Agarwal <84928524+khyatisketch@users.noreply.github.com>
2024-04-25 09:55:31 +0530

    first commit

---
+++ netflix/src/utils/constant.js
@@ -0,0 +1,17 @@
+export const API_END_POINT = "http://localhost:8080/api/v1/user";
+
+export const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZDAyNjJhZjcxODAxNDYwNjMzMDVkYmViODM1M2M5NiIsInN1YiI6IjY2Mjc4OGZjYWY5NTkwMDE3ZDZiYTk2MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0mkT1Q_0C8WR4Dwox2rVFz7KP5J0pU1hR2LIhMhYwu0'
+    }
+};
+export const Now_Playing_Movie = "https://api.themoviedb.org/3/movie/now_playing";
+export const Popular_Movie = "https://api.themoviedb.org/3/movie/popular";
+export const Top_Rated_Movie = "https://api.themoviedb.org/3/movie/top_rated";
+export const Upcoming_Movie = "https://api.themoviedb.org/3/movie/upcoming";
+
+export const SEARCH_MOVIE_URL = "https://api.themoviedb.org/3/search/movie?query=";
+
+export const TMDB_IMG_URL = "https://image.tmdb.org/t/p/w500";
